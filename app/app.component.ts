714e19a81363cd4ad0a683e2dadcd5da33f88350
Richard Pablo <richardpablop@gmail.com>
2017-06-25 23:44:46 +0100

    Chapter 3 - Adding structure, signup, login, services, routes and list page

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,19 +1,7 @@
 import { Component } from "@angular/core";
 
 @Component({
-  selector: "my-app",
-  template: `
-  <StackLayout>
-    <!-- res:// references to any of the images inside the app/App_Resources folder -->
-    <Image src="res://logo_login" stretch="none" horizontalAlignment="center"></Image>
-    <TextField hint="Email Address" keyboardType="email"
-      autocorrect="false" autocapitalizationType="none"></TextField>
-    <TextField hint="Password" secure="true"></TextField>
-
-    <Button text="Sign in" class="submit-button"></Button>
-    <Button text="Sign up for Groceries"></Button>
-  </StackLayout>
-  `,
-  styleUrls: ["pages/login/login-common.css", "pages/login/login.css"]
+  selector: "main",
+  template: "<page-router-outlet></page-router-outlet>"
 })
 export class AppComponent {}
