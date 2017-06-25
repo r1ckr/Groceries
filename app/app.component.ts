e675ed523e7286c1b06d3be48eac0f9df552323c
Richard Pablo <richardpablop@gmail.com>
2017-06-25 22:18:48 +0100

    Chaper 2 - Adding Login screen, styles and images

---
+++ app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "my-app",
+  template: `
+  <StackLayout>
+    <!-- res:// references to any of the images inside the app/App_Resources folder -->
+    <Image src="res://logo_login" stretch="none" horizontalAlignment="center"></Image>
+    <TextField hint="Email Address" keyboardType="email"
+      autocorrect="false" autocapitalizationType="none"></TextField>
+    <TextField hint="Password" secure="true"></TextField>
+
+    <Button text="Sign in" class="submit-button"></Button>
+    <Button text="Sign up for Groceries"></Button>
+  </StackLayout>
+  `,
+  styleUrls: ["pages/login/login-common.css", "pages/login/login.css"]
+})
+export class AppComponent {}
